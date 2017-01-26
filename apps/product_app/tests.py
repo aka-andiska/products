7787b8669df5b03117d7a46aa51f5c9bd80531ee
aka-andiska <aka.afenandiska@tampinestechnology.com>
2017-01-26 10:31:56 +0800

    create index test

--- apps/product_app/tests.py
+++ apps/product_app/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import Product
+
+from django.core.urlresolvers import reverse
+
+class PeroductMethodTest(TestCase):
+    def test_index(self):
+        """
+        Index should display all peoples
+        """
+        table = Product.objects.create(name="table", description="long table") #this how to arrange
+        chair = Product.objects.create(name="chair", description="office chair")
+
+        response = self.client.get(reverse('index')) #this is how to act
+
+        self.assertEqual(response.status_code, 200) #this is how to assert
+        self.assertEqual(len(response.context['products']),2)
+        self.assertEqual(response.context['products'][0], table)
+        self.assertEqual(response.context['products'][1], chair)
 
 # Create your tests here.
--- apps/product_app/urls.py
+++ apps/product_app/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^$', views.index, name='index'),
     url(r'^create$', views.create),
     url(r'^edit/(?P<id>\d+)$', views.edit),
     url(r'^update/(?P<id>\d+)$', views.update),
