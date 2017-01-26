5ced5f555dbc801b0baf1af85a02d20d359f9434
aka-andiska <aka.afenandiska@tampinestechnology.com>
2017-01-26 10:38:04 +0800

    setting urls to create test

--- apps/product_app/urls.py
+++ apps/product_app/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^create$', views.create, name='create'),
-    url(r'^edit/(?P<id>\d+)$', views.edit),
-    url(r'^update/(?P<id>\d+)$', views.update),
-    url(r'^delete/(?P<id>\d+)$', views.destroy)
+    url(r'^edit/(?P<id>\d+)$', views.edit, name='edit'),
+    url(r'^update/(?P<id>\d+)$', views.update, name='update'),
+    url(r'^delete/(?P<id>\d+)$', views.destroy, name='delete')
 ]
 
