717653a246567e1799f8cca949a12e4bb2f78995
aka-andiska <aka.afenandiska@tampinestechnology.com>
2017-01-26 08:52:30 +0800

    setting models and timezone

--- apps/product_app/models.py
+++ apps/product_app/models.py
@@ -6,5 +6,5 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
