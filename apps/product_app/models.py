3ceb6fd22d93398ab055b497cd6c498f1de8168f
aka-andiska <aka.afenandiska@tampinestechnology.com>
2017-02-16 18:22:26 +0700

    create model category

--- apps/product_app/models.py
+++ apps/product_app/models.py
@@ -2,6 +2,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    information = models.TextField(default="Normal category", null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
